ea6b277836f699176d48593c4f57b5c2c5e71e66
nilSinger <niilz@outlook.de>
2021-08-16 22:06:42 +0200

    Added the addRegistration post-endpoint with pseudo-code-body

--- src/main/java/com/pluralsight/conference/controller/AccountController.java
+++ src/main/java/com/pluralsight/conference/controller/AccountController.java
@@ -1,8 +1,14 @@
 package com.pluralsight.conference.controller;
 
+import javax.validation.Valid;
+
+import com.pluralsight.conference.model.Account;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AccountController {
@@ -10,5 +16,22 @@ public class AccountController {
 	@GetMapping("/account")
 	public String getRegistration(@ModelAttribute("account") Account account) {
 		return "account";
+	}
+
+	@PostMapping("/account")
+	public String addRegistration(@Valid @ModelAttribute("account") Account
+			account, BindingResult result) {
+
+		// check for errors
+		// should verify that account and user does not exist
+		// should verify that email is correct
+
+		// encrypt password
+		
+		// create the account
+
+		// fire off event on creation
+		
+		return "redirect:account";
 	}
 }
