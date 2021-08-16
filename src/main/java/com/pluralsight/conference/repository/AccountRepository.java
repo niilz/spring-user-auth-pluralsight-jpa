d3bdcf202e59f48646c24fa556481c32edc6580d
nilSinger <niilz@outlook.de>
2021-08-16 22:52:22 +0200

    Added the repository interface jpa-style (not working yet)

---
+++ src/main/java/com/pluralsight/conference/repository/AccountRepository.java
@@ -0,0 +1,12 @@
+package com.pluralsight.conference.repository;
+
+import com.pluralsight.conference.model.Account;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+public interface AccountRepository extends CrudRepository<Account, String> {
+
+	public Account create(Account account);
+
+}
