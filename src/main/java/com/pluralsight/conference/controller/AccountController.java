1eddba56653b807b4ed4647507a7a766ece3c8b2
nilSinger <niilz@outlook.de>
2021-08-16 21:35:08 +0200

    Corrected type in AccountController's package declaration

--- src/main/java/com/pluralsight/conference/controller/AccountController.java
+++ src/main/java/com/pluralsight/conference/controller/AccountController.java
@@ -1,4 +1,4 @@
-package com.pluralsigh.conference.controller;
+package com.pluralsight.conference.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
