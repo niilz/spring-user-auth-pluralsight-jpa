e9cdea1393588f71de826dd6e354f8f16868a304
nilSinger <niilz@outlook.de>
2021-08-16 21:16:38 +0200

    Commented out LDAP and reenabled jdbc

--- src/main/java/com/pluralsight/conference/ConferenceSecurityConfig.java
+++ src/main/java/com/pluralsight/conference/ConferenceSecurityConfig.java
@@ -71,8 +71,11 @@ public class ConferenceSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-        // auth.jdbcAuthentication().dataSource(dataSource);
+        auth.jdbcAuthentication()
+            .dataSource(dataSource)
+            .passwordEncoder(passwordEncoder());
         
+        /*
         auth.ldapAuthentication()
             .userDnPatterns("uid={0},ou=people") // DN: Distinguished Name, ou: organization unit
             .groupSearchBase("ou=groups")
@@ -84,6 +87,7 @@ public class ConferenceSecurityConfig extends WebSecurityConfigurerAdapter {
             .passwordAttribute("userPassword")
             .and()
             .userDetailsContextMapper(ctxMapper);
+        */
     }
 
     @Bean
