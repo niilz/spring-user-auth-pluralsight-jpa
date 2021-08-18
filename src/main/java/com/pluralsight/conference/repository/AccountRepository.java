34f02b6d1893dfdb6c2e0e6684013ab4eb0d94a1
nilSinger <niilz@outlook.de>
2021-08-18 22:31:40 +0200

    Removed the save-method declaration, since it is implicitly available

--- src/main/java/com/pluralsight/conference/repository/AccountRepository.java
+++ src/main/java/com/pluralsight/conference/repository/AccountRepository.java
@@ -3,10 +3,6 @@ package com.pluralsight.conference.repository;
 import com.pluralsight.conference.model.Account;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 public interface AccountRepository extends CrudRepository<Account, String> {
-
-	public Account save(Account account);
-
 }
