b785be404538028fce2c2c1c531489a01f038b62
nilSinger <niilz@outlook.de>
2021-10-01 21:31:54 +0200

    Added the PasswordListener (does not compile yet: PasswordService is missing)

---
+++ src/main/java/com/pluralsight/conference/util/PasswordListener.java
@@ -0,0 +1,43 @@
+package com.pluralsight.conference.util;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PasswordListener implements ApplicationListener<OnPasswordResetEvent> {
+
+	private String serverUrl = "http://localhost:8080/";
+
+	@Autowired
+	private JavaMailSender mailSender;
+
+	@Override
+	public void onApplicationEvent(OnPasswordResetEvent event) {
+		this.resetPassword(event);
+	}
+
+	private void resetPassword(OnPasswordResetEvent event) {
+		// Create Password-Token
+		var password = event.getPassword();
+		var token = UUID.randomUUID().toString();
+		passwordService.createResetToken(password, token);
+		// Get Email Properties
+		var recipientAddress = password.getEmail();
+		var subject = "Reset Password";
+		var confirmationUrl = event.getAppUrl() +	"/passwordReset?token=" + token;
+		var message = "Reset password:";
+		// send Email
+		var email = new SimpleMailMessage();
+		email.setTo(recipientAddress);
+		email.setSubject(subject);
+		email.setText(message + "\r\n" + serverUrl + confirmationUrl);
+		mailSender.send(email);
+
+	}
+
+}
