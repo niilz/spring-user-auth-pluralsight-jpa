b0d476441b52fa8c6c822a058741f7bc8765fc24
nilSinger <niilz@outlook.de>
2021-10-14 21:56:28 +0200

    Renamed all userName occurances to username in Password

--- src/main/java/com/pluralsight/conference/model/Password.java
+++ src/main/java/com/pluralsight/conference/model/Password.java
@@ -16,7 +16,7 @@ public class Password {
 	
 	@NotNull
 	@NotEmpty
-	private String userName;
+	private String username;
 	private String token;
 
 	public String getPassword() {
@@ -37,11 +37,11 @@ public class Password {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	public String getUserName() {
-		return userName;
+	public String getUsername() {
+		return username;
 	}
-	public void setUserName(String userName) {
-		this.userName = userName;
+	public void setUsername(String username) {
+		this.username = username;
 	}
 	public String getToken() {
 		return token;
